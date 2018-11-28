925fd44c579a4397ed8e0f2334fdc0ff5087f37c
Garrett Morton <gemorton@umich.edu>
2018-11-28 10:39:51 -0500

    ec 1 implementation

--- top_headlines.py
+++ top_headlines.py
@@ -11,15 +11,27 @@ def index():
 
 @app.route('/user/<nm>')
 def user_name(nm):
-	headlines = get_headlines("Technology")
-	return render_template('user.html', name=nm, headlines=headlines)
+	section = "Technology"
+	headlines = get_headlines(section)
+	return render_template('user.html', name=nm, headlines=headlines, section=section)
+
+@app.route('/user/<nm>/<sec>')
+def ec_1(nm, sec):
+	section = sec.capitalize()
+	headlines = get_headlines(section)
+	return render_template('user.html', name=nm, headlines=headlines, section=section)
 
 def get_headlines(section):
 	r_format = "json"
-	baseurl = "https://api.nytimes.com/svc/mostpopular/v2/mostviewed/{}/7.{}".format(section, r_format)
+	baseurl = "https://api.nytimes.com/svc/mostpopular/v2/mostviewed/{}/7.{}".format(section, r_format) #mostpopular API
+	#baseurl = "https://api.nytimes.com/svc/topstories/v2/{}.{}".format(section, r_format) #topstories API (not working--"access denied"?)
 	params = {"api-key":apikey}
 
 	r = requests.get(baseurl, params)
+	print(r.url)
+	f = open('out.txt', 'w')
+	f.write(r.text)
+	f.close()
 	json_data = json.loads(r.text)
 
 	results = json_data["results"]
